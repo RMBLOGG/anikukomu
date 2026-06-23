a05903cdb710b2cdd9305a4c86c2f0134ee12c03
RMBLOGG <ramdanhidayah176@gmail.com>
2026-06-23 08:12:26 +0700

    memek

--- app/src/main/java/com/dayynime/anikukomu/core/Constants.kt
+++ app/src/main/java/com/dayynime/anikukomu/core/Constants.kt
@@ -2,7 +2,7 @@ package com.dayynime.anikukomu.core
 
 object Constants {
     const val SUPABASE_URL = "https://sfyyljfpnutskeenlysb.supabase.co"
-    const val SUPABASE_KEY = "sb_publishable_hCyajVO9ZXZEnsoXK2L_eA_RTg7K6K-"
+    const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmeXlsamZwbnV0c2tlZW5seXNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDAzNTcsImV4cCI6MjA5NzcxNjM1N30.Qaecy5q6GyI_pbG-LgtzqXJrTtu6AXMshA20P20n2pc"
 
     const val CLOUDINARY_CLOUD_NAME = "dzfkklsza"
     const val CLOUDINARY_API_KEY = "588474134734416"
